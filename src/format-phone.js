2af6535bab5da9fcc9cc758bf769dffd56a4a93f
Ian Storz <istorz@nestlabs.com>
2017-05-04 13:40:34 -0700

    fix

--- src/format-phone.js
+++ src/format-phone.js
@@ -2,35 +2,3 @@
 function formatPhone(number) {
   return number;
 }
-
-// Comment the previous function and
-// Uncomment this function to pass test
-/*
-function formatPhone(number) {
-  var string = number.toString();
-  var firstSegment = string.slice(0,3);
-  var secondSegment = string.slice(3,7);
-  
-  return firstSegment + '-' + secondSegment;
-}
-*/
-
-// Comment the previous functions and
-// Uncomment this function to extend
-// Functionality to match new tests
-/*
-function formatPhone(number) {
-  var string = number.toString();
-
-  if (string.length == 7) {
-    var firstSegment = string.slice(0,3);
-    var secondSegment = string.slice(3,7);
-    return firstSegment + '-' + secondSegment;
-  } else if (string.length == 10) {
-    var areaCode = string.slice(0,3);
-    var firstSegment = string.slice(3,6);
-    var secondSegment = string.slice(6,10);
-    return '(' + areaCode + ')' + firstSegment + '-' + secondSegment;
-  }
-}
-*/
--- src/tests.js
+++ src/tests.js
@@ -1,10 +1,3 @@
 QUnit.test("formatPhone correctly formats numbers", function(assert) {
   assert.equal(formatPhone(5555555), '555-5555');
-  assert.ok(formatPhone(5555555) === '555-5555');
-// Uncomment the following asserts to expand functionality
-//  assert.equal(formatPhone(1234567890), '(123)456-7890');
-//  assert.ok(formatPhone(1234567890) === '(123)456-7890');
-// Don't forget some negative tests!
-//  assert.ok(formatPhone(123) === undefined);
-//  assert.ok(formatPhone(12345678901) === undefined);
 });
